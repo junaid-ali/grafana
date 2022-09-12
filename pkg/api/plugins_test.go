bb089050044a79f58cdd4ee8702703d6cb3617ec
gamab <gabi.mabs@gmail.com>
2022-09-12 11:09:35 +0200

    Register resolver in test

--- pkg/api/plugins_test.go
+++ pkg/api/plugins_test.go
@@ -483,6 +483,7 @@ func Test_PluginsList_AccessControl(t *testing.T) {
 		sc.hs.PluginSettings = &pluginSettings
 		sc.hs.pluginStore = pluginStore
 		sc.hs.pluginsUpdateChecker = updatechecker.ProvidePluginsService(sc.hs.Cfg, pluginStore)
+		sc.hs.AccessControl.RegisterScopeAttributeResolver(plugins.NewIDScopeResolver(sc.hs.pluginStore))
 		setInitCtxSignedInUser(sc.initCtx, testUser(tc.role, tc.isGrafanaAdmin))
 
 		t.Run(testName(tc), func(t *testing.T) {
